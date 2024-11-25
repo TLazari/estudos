cc90db7aa62ca7c64f13e9afa38c0268fdf221ba
TLazari <thiagolazari123@hotmail.com>
2024-11-25 12:54:56 -0300

    Calculadora simples singles thread

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+valor1 = input("Digite o primeiro valor: ")
+valor2= input("Digite o segundo valor: ")
+
+soma = int(valor1) + int(valor2)
+subtracao = int(valor1) - int(valor2)
+multiplicacao = int(valor1) * int(valor2)
+divisao = int(valor1) / int(valor2)
+
+print ("Soma: ", soma)
+print ("Subtração: ", subtracao)
+print ("Multiplicação: ", multiplicacao)
+print ("Divisão: ", divisao)
