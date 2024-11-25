4e8792cbf77e474cf41ed7caadd910eeed237453
TLazari <thiagolazari123@hotmail.com>
2024-11-25 13:20:53 -0300

    Um thread para cada operação

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,40 @@
+import threading
+import time
+
+def soma():
+    resultado = int(valor1) + int(valor2)
+    print("Soma: ", resultado)
+
+def subtracao():
+    resultado = int(valor1) - int(valor2)
+    print("Subtração: ", resultado)
+
+def multiplicacao():
+    resultado = int(valor1) * int(valor2)
+    print("Multiplicação: ", resultado)
+
+def divisao():
+    resultado = int(valor1) / int(valor2)
+    print("Divisão: ", resultado)
+
+
 valor1 = input("Digite o primeiro valor: ")
-valor2= input("Digite o segundo valor: ")
+valor2 = input("Digite o segundo valor: ")
 
-soma = int(valor1) + int(valor2)
-subtracao = int(valor1) - int(valor2)
-multiplicacao = int(valor1) * int(valor2)
-divisao = int(valor1) / int(valor2)
+thread1 = threading.Thread(target=soma)
+thread2 = threading.Thread(target=subtracao)
+thread3 = threading.Thread(target=multiplicacao)
+thread4 = threading.Thread(target=divisao)
 
-print ("Soma: ", soma)
-print ("Subtração: ", subtracao)
-print ("Multiplicação: ", multiplicacao)
-print ("Divisão: ", divisao)
+inicio = time.perf_counter()
+threads = [thread1, thread2, thread3, thread4]
+
+for thread in threads:
+    thread.start()
+    thread.join()
+
+fim_tempo = time.perf_counter()
+tempo_execucao = (fim_tempo - inicio) * 1_000
+
+print(f"Tempo de execução: {(tempo_execucao):.2f} milissegundos")
+print("Fim da execução")
